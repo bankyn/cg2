f8d3cf4ef52f0076d6a7f74e88748ea0774ed0b3
dHeyne <c.loc@hotmail.de>
2013-04-23 12:16:26 +0200

    functions draw and isHit implemented, constructor not finished
    Signed-off-by: dHeyne <c.loc@hotmail.de>

--- cg2-a01/parametric_curve.js
+++ cg2-a01/parametric_curve.js
@@ -13,11 +13,20 @@ define(["util", "vec2", "scene", "point_dragger", "straight_line"],
 		this.segments = segments || 20;
 		this.tickmarks = tickmarks || false;
 		this.lines = new Array(this.segments);
+		//straight line objekte ins array speichern,
+		//hier wird das konstrukt zum zeichnen erstellt
+	};
+	
+	ParametricCurve.prototype.isHit() {
+		for(var _index=0; _index < this.segments; _index++) {
+			if(this.lines[_index].isHit) return true;
+		}
+		return false;
 	};
 	
 	ParametricCurve.prototype.draw = function(context) {
 		for(var _index=0; _index < this.segments; _index++) {
-			
+			this.lines[_index].draw(context);
 		}
 	};
 	
